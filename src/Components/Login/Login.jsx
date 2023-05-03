f09e457f041021febf66d13dd066a9dbc9aefea0
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-03 09:27:51 +0600

    some component styled newly

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -15,18 +15,21 @@ function Login() {
         <div className='container mx-auto text-center'>
             <Header></Header>
             <form onSubmit={handleSubmit} className='items-center p-6 text-center bg-slate-100'>
-            <h2 className='mb-2 font-bold text-purple-700'>Please Login!!!</h2>
+                <h2 className='mb-2 font-bold text-purple-700'>Please Login!!!</h2>
                 <label className="flex justify-center input-group">
                     <span>Email</span>
-                    <input type="email" placeholder="Your Email" className="shadow-lg input input-bordered" />
+                    <input type="email" placeholder="Your Email" className="shadow-lg input input-bordered" required />
                 </label>
-                <label className="flex justify-center mt-3 input-group">
-                    <span>Password</span>
-                    <input type="password" placeholder="Your password" className="shadow-lg input input-bordered" />
-                </label>
-               
-                <button className='px-5 py-3 mt-3 font-bold text-white bg-purple-700 rounded-sm shadow-lg' type="submit">Login</button>
-            <p className='mt-2'>Don't have any account? <Link className='font-bold text-blue-800' to="/register">Please Register</Link> </p>
+                    <label className="flex justify-center mt-3 input-group">
+                        <span>Password</span>
+                        <input type="password" placeholder="Your password" className="shadow-lg input input-bordered" required />
+                    </label>
+
+                <div className='form-control'>
+                    <button className='px-5 py-3 mt-3 font-bold text-white bg-purple-700 rounded-sm shadow-lg' type="submit">Login</button>
+                </div>
+
+                <p className='mt-2'><small>Don't have any account? <Link className='font-bold text-blue-800' to="/register">Please Register</Link></small> </p>
             </form>
             <Footer></Footer>
         </div>
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -5,29 +5,36 @@ import Header from '../Shared/Header/Header';
 import Footer from '../Shared/Footer/Footer';
 
 function Register() {
-    const handleRegister = () =>{
+    const handleRegister = () => {
         event.preventDefault()
     }
     return (
         <div className='container mx-auto text-center'>
             <Header></Header>
             <form onSubmit={handleRegister} className='items-center p-6 text-center bg-slate-100'>
-            <h2 className='mb-2 font-bold text-purple-700'>Please Register!!!</h2>
+                <h2 className='mb-2 font-bold text-purple-700'>Please Register!!!</h2>
                 <label className="flex justify-center mb-3 input-group">
                     <span>Name</span>
-                    <input type="text" placeholder="Your Name" className="shadow-lg input input-bordered" />
+                    <input type="text" placeholder="Your Name" className="shadow-lg input input-bordered" required />
                 </label>
+                <label className="flex justify-center mb-3 input-group">
+                    <span>Photo URL</span>
+                    <input type="text" placeholder="Your photo URL" className="shadow-lg input input-bordered" required />
+                </label>
+
                 <label className="flex justify-center input-group">
                     <span>Email</span>
-                    <input type="email" placeholder="Your Email" className="shadow-lg input input-bordered" />
+                    <input type="email" placeholder="Your Email" className="shadow-lg input input-bordered" required />
                 </label>
                 <label className="flex justify-center mt-3 input-group">
                     <span>Password</span>
-                    <input type="password" placeholder="Your password" className="shadow-lg input input-bordered" />
+                    <input type="password" placeholder="Your password" className="shadow-lg input input-bordered" required />
                 </label>
-               
-                <button className='px-5 py-3 mt-3 font-bold text-white bg-purple-700 rounded-sm shadow-lg' type="submit">Register</button>
-            <p className='mt-2'>Have an account? <Link className='font-bold text-blue-800' to="/login">Please Login</Link> </p>
+
+                <div className='form-control'>
+                    <button className='px-5 py-3 mt-3 font-bold text-white bg-purple-700 rounded-sm shadow-lg' type="submit">Register</button>
+                </div>
+                <p className='mt-2'><small>Have an account? <Link className='font-bold text-blue-800' to="/login">Please Login</Link></small> </p>
             </form>
             <Footer></Footer>
         </div>
