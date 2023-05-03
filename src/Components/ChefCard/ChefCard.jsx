8151e1e5901bb8a29f1734adf14375b04da03c9e
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-03 14:56:54 +0600

    see recipe button added

--- src/Components/ChefCard/ChefCard.jsx
+++ src/Components/ChefCard/ChefCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaHeart } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 const ChefCard = ({data}) => {
     const {chef_name, years_of_experience, num_recipes, num_likes, chef_img} = data;
     return (
@@ -8,8 +9,10 @@ const ChefCard = ({data}) => {
             <h2><span className='font-bold'>Chef Name:</span> {chef_name}</h2>
             <p><span className='font-bold'>Experience:</span> {years_of_experience} Years</p>
             <p><span className='font-bold'>Recipe:</span> {num_recipes} items</p>
-            <p className='flex items-center gap-3'><span className='text-purple-700'><FaHeart></FaHeart></span> {num_likes}</p>
-
+            <p className='flex items-center gap-3'><span className='text-red-700'><FaHeart></FaHeart></span> {num_likes}</p>
+        
+            <button className='px-5 py-2 text-white bg-purple-700 hover:bg-purple-950'><Link>See Recipe</Link></button>
+            
         </div>
     );
 };
