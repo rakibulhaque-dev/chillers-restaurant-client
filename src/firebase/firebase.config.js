41c2ef8564942d8a3f49f46ea84ca2e3472c173a
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-04 23:31:18 +0600

    Environmnet Cleared

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('environment variable', import.meta.env.VITE_apiKey)
 const firebaseConfig = {
-  apiKey: "AIzaSyDdunYtHTx3X6C-lCdcXBGAddUu5OmI71Q",
-  authDomain: "chillers-restaurant.firebaseapp.com",
-  projectId: "chillers-restaurant",
-  storageBucket: "chillers-restaurant.appspot.com",
-  messagingSenderId: "21426658806",
-  appId: "1:21426658806:web:1a3aa971062f5e934e3bd7"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.VITE_APPID,
 };
 
 // Initialize Firebase
