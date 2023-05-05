15c5c5435e4039809bca82dca2ba6bd96c6c5172
rakibulhaqueonline <rakibulhaque.info@gmail.com>
2023-05-05 16:17:02 +0600

    Header Modified

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import footerImage from '../../assets/images/footer.jpg';
+import foodImage from '../../assets/images/foods.png';
 function Banner() {
     return (
-        <div className=''>
-            <div className="min-h-screen mb-3 shadow-lg hero" style={{ backgroundImage: `url(${footerImage})` }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="text-center hero-content text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold text-purple-300">Welcome to American Foods</h1>
-                        <p className="mb-5">
+        <div className='container mx-auto border rounded-lg shadow-lg'>
+            <div className="items-center justify-center gap-4 p-6 sm:flex-row-reverse md:flex lg:flex sm:text-center">
+                <div className='mr-5 text-left'>
+                    <h1 className="mb-5 text-5xl font-bold text-pink-500">Welcome to American Foods</h1>
+                    <p className="mb-5 text-black">
                         American cuisine consists of the cooking style and traditional dishes prepared in the United States. It has been significantly influenced by Europeans, indigenous Native Americans, Africans, Asians, Pacific Islanders, and many other cultures and traditions.</p>
-                    </div>
+                </div>
+                <div>
+                    <img className='justify-center w-auto h-auto mx-auto text-center' src={foodImage} alt="" />
                 </div>
             </div>
         </div>
--- src/Components/Shared/Header/Header.jsx
+++ src/Components/Shared/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react'
 import { AuthContext } from '../../../provider/AuthProvider';
 import { FaUser, FaBars, FaTimes } from 'react-icons/fa';
+import LogoRest from '../../../assets/images/chillers-logo.png'
+
 
 const Header = () => {
   const { user, logOut } = useContext(AuthContext)
@@ -19,7 +21,10 @@ const Header = () => {
   return (
     <div className='container items-center justify-between p-5 mx-auto mb-4 shadow-lg md:flex'>
       <div className='flex items-center justify-between'>
-        <h3 className='text-2xl font-extrabold'><span className='text-purple-800'>CHILL</span>ERS <span className='text-purple-800'>REST</span>AURANT</h3>
+        <div className='flex items-center gap-3'>
+          <img className='w-24 h-20' src={LogoRest} alt="" />
+            <p className='text-2xl'>Chillers Restaurant</p>
+        </div>
         <div className='md:hidden'>
           {isMenuOpen ? (
             <FaTimes onClick={toggleMenu} className='text-2xl cursor-pointer' />
@@ -35,21 +40,22 @@ const Header = () => {
         <li><a className='block mt-4 font-bold text-purple-600 md:inline-block md:mt-0 md:ml-6' href="/register">Register</a></li>
         <li><a className='block mt-4 font-bold text-purple-600 md:inline-block md:mt-0 md:ml-6' href="/login">Login</a></li>
         <li><a className='block mt-4 font-bold text-purple-600 md:inline-block md:mt-0 md:ml-6' href="/chef">Chef</a></li>
-      </ul>
 
-      <div className='flex items-center justify-center gap-4'>
-        {
-          user?.photoURL ? <img className='w-8 h-8 rounded-full' src={user.photoURL} alt="" />
-            : ""}
-        {
-          user?.displayName ? <span>{user.displayName}</span> : ""
-        }
-        <small>
-          {user &&
-            <button className='px-2 py-1 text-white bg-purple-500 rounded-sm hover:bg-purple-900' onClick={handleLogOut}>Log Out</button>
+
+        <div className='flex items-center justify-center gap-4'>
+          {
+            user?.photoURL ? <img className='w-8 h-8 rounded-full ml-14' src={user.photoURL} alt="" />
+              : ""}
+          {
+            user?.displayName ? <span>{user.displayName}</span> : ""
           }
-        </small>
-      </div>
+          <small>
+            {user &&
+              <button className='px-2 py-1 text-white bg-purple-500 rounded-sm hover:bg-purple-900' onClick={handleLogOut}>Log Out</button>
+            }
+          </small>
+        </div>
+      </ul>
     </div>
   )
 }
